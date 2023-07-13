0f1ea5d65d1254e2bca82cdc8f0c053489cf8eef
SG_COMPUTER <rlagusrl928@gmail.com>
2023-07-13 14:36:11 +0900

    test

--- src/main/java/com/example/github/cherrypick/PrTestClass.java
+++ src/main/java/com/example/github/cherrypick/PrTestClass.java
@@ -1,35 +1,5 @@
 package com.example.github.cherrypick;
 
 public class PrTestClass {
-    /*여러개의 PR을 분리해 올리는 작업 연습*/
 
-    /*commit 1*/
-    public void commit1() {
-        System.out.println("PR1");
-    }
-
-    /*commit 2*/
-    public void commit2() {
-        System.out.println("PR2");
-    }
-
-    /*commit 3*/
-    public void commit3() {
-        System.out.println("PR3");
-    }
-
-    /*commit 4*/
-    public void commit4() {
-        System.out.println("PR4");
-    }
-
-    /*commit 5*/
-    public void commit5() {
-        System.out.println("PR5");
-    }
-
-    /*commit 6*/
-    public void commit6() {
-        System.out.println("PR6");
-    }
 }
