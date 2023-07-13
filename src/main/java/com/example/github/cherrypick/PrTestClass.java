e94b85187bc67fd8cec6c347f1bc49c2f1d8fcc7
SG_COMPUTER <rlagusrl928@gmail.com>
2023-07-13 14:46:17 +0900

    1

--- src/main/java/com/example/github/cherrypick/PrTestClass.java
+++ src/main/java/com/example/github/cherrypick/PrTestClass.java
@@ -1,14 +1,7 @@
 package com.example.github.cherrypick;
 
 public class PrTestClass {
-    /*commit 1*/
-    private void commit(){
-        System.out.println("commit 1");
-    }
 
-    /*commit 2*/
-    private void commit2(){
-        System.out.println("commit 2");
-    }
+    // commit - 1
 
 }
