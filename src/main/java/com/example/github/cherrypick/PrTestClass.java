16b798f6988d695a48a785c45a06911a576a4f75
SG_COMPUTER <rlagusrl928@gmail.com>
2023-07-13 14:34:48 +0900

    commit 2

--- src/main/java/com/example/github/cherrypick/PrTestClass.java
+++ src/main/java/com/example/github/cherrypick/PrTestClass.java
@@ -3,5 +3,9 @@ package com.example.github.cherrypick;
 public class PrTestClass {
     /*여러개의 PR을 분리해 올리는 작업 연습*/
 
+    /*commit 1*/
+    public void commit1() {
+        System.out.println("PR1");
+    }
 
 }
